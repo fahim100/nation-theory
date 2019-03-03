a8e35a19435c2945a4919155e3750ed3bfd55d22
fahim100 <fahimahmmed100@gmail.com>
2019-03-03 20:49:53 +0600

    Fist commit on this project.

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    'use strict';
+    $('.nat-case-study-slider-left').slick({
+        slideToShow: 1,
+        dots: true,
+        arrows: true,
+        prevArrow: "<div class='nat-slider-prev'><i class='fa fa-angle-left'></i></div>",
+        nextArrow: "<div class='nat-slider-next'><svg width='35' height='7'><path fill-rule='evenodd' opacity='.8' d='M35 7L0 6.999V4.846h28.406V0L35 7z'/></svg></div>",
+        customPaging : function(slider, i) {
+            var title = $(slider.$slides[i]).data('title');
+            return '<a class="pager__item"> '+title+' </a>';
+        },
+    });
+    $('.nat-case-study-slider-right').slick({
+        slideToShow: 1,
+        dots: true,
+        arrows: true,
+        prevArrow: "<div class='nat-slider-prev'><i class='fa fa-angle-left'></i></div>",
+        nextArrow: "<div class='nat-slider-next'><svg width='35' height='7'><path fill-rule='evenodd' opacity='.8' d='M35 7L0 6.999V4.846h28.406V0L35 7z'/></svg></div>",
+        customPaging : function(slider, i) {
+            var title = $(slider.$slides[i]).data('title');
+            return '<a class="pager__item"> '+title+' </a>';
+        },
+    });
+});

dbb7d82be45be6ff45036534fe38c8892eb5d188
fahim100 <fahimahmmed100@gmail.com>
2019-03-06 16:30:44 +0600

    Fixed naming conversion every section.

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     'use strict';
-    $('.nat-case-study-slider-left').slick({
+    $('#nat__case__slider-left').slick({
         slideToShow: 1,
         dots: true,
-        arrows: true,
+        arrows: false,
         prevArrow: "<div class='nat-slider-prev'><i class='fa fa-angle-left'></i></div>",
         nextArrow: "<div class='nat-slider-next'><svg width='35' height='7'><path fill-rule='evenodd' opacity='.8' d='M35 7L0 6.999V4.846h28.406V0L35 7z'/></svg></div>",
         customPaging : function(slider, i) {
@@ -11,10 +11,10 @@ $(document).ready(function () {
             return '<a class="pager__item"> '+title+' </a>';
         },
     });
-    $('.nat-case-study-slider-right').slick({
+    $('#nat__case__slider-right').slick({
         slideToShow: 1,
         dots: true,
-        arrows: true,
+        arrows: false,
         prevArrow: "<div class='nat-slider-prev'><i class='fa fa-angle-left'></i></div>",
         nextArrow: "<div class='nat-slider-next'><svg width='35' height='7'><path fill-rule='evenodd' opacity='.8' d='M35 7L0 6.999V4.846h28.406V0L35 7z'/></svg></div>",
         customPaging : function(slider, i) {
